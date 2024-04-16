365d280b21d20dc2abf39d6726a8c1062dc0834a
Calvin <calvinsoares17@gmail.com>
2024-04-15 22:05:51 -0300

    feat: rotas do usuario

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { createUser, loginUser } from "../controllers/user.controller";
+
+const userRouter = express.Router();
+
+userRouter.post("/create", createUser);
+userRouter.post("/login", loginUser);
+
+export default userRouter;
